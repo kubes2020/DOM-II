888d09f6925e62e8a422ed585ba2d175064f3869
Brian Kubes <briankubes@gmail.com>
2020-09-01 14:30:12 -0700

    half way done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const intro = document.querySelector('.intro');
+
+intro.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'red';
+})
+
+intro.addEventListener('mouseout', (event) => {
+    event.target.style.color = 'black';
+})
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'b')
+    console.log('you performed a key press on b')
+});
+
+
+const imgBig = document.querySelector('.img-content');
+imgBig.addEventListener('dblclick', function (event) {
+    event.target.style.width = "50%";
+})
+
+const mybutton = document.querySelector('#mybutton');
+mybutton.addEventListener('focus', (event) => {
+    event.target.style.backgroundColor = 'yellow';
+});
---
+++ stretch-assignment/index.js
@@ -0,0 +1,12 @@
+const intro = document.querySelector('.intro');
+
+intro.addEventListener('mouseover', (event) => {
+    debugger
+    event.target.style.color = 'red';
+})
+
+console.log('hello')
+
+const keyDown = document.querySelector('document');
+
+keyDown
