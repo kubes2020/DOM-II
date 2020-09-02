f6034b148c6c6f794d225f7bc691da158576a789
Brian Kubes <briankubes@gmail.com>
2020-09-01 20:00:47 -0700

    pretty much done

--- js/index.js
+++ js/index.js
@@ -25,3 +25,42 @@ const mybutton = document.querySelector('#mybutton');
 mybutton.addEventListener('focus', (event) => {
     event.target.style.backgroundColor = 'yellow';
 });
+
+const advAwaits = document.querySelector('.text-content:nth-of-type(2)');
+
+window.addEventListener('resize', (event) => {
+    advAwaits.style.border = '2px solid green';
+})
+
+
+const introImg = document.querySelector('.intro img')
+introImg.addEventListener('wheel', function(){
+    alert("slow down")
+})
+
+
+const selectC = document.querySelector('#mybutton');
+
+selectC.addEventListener('select', (event) => {
+    event.target.style.color = 'teal';
+})
+
+window.addEventListener('load', (event) => {
+    console.log('You did it!');
+})
+
+window.addEventListener('scroll', (event) => {
+    console.log('scroll')
+})
+
+const navTop = document.querySelector('.nav-container');
+const logo = document.querySelector('.logo-heading');
+navTop.addEventListener('click', (event) => {
+    event.target.style.background = 'purple';
+    logo.addEventListener('click', (event) => {
+        event.stopPropagation()
+    })
+
+})
+
+
